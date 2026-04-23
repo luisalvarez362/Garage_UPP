06a7065c7c5e2e39b81e8d5f22ef6f9c9be4ee02
luisalvarez362 <luis.alvarez362@uppuebla.edu.mx>
2026-04-23 11:51:34 -0600

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_garage_proyect (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,26 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    assign uo_out[2]=0;
+    assign uo_out[3]=0;
+    assign uo_out[4]=0;
+    assign uo_out[5]=0;
+    assign uo_out[6]=0;
+    assign uo_out[7]=0;
 
+    assign uio_oe = 8'b1111_1111;
+    assign uio_out[7:0] = 8'b0000_0000;
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
-
+    wire _unused = &{ui_in[4], ui_in[5], ui_in[6],ui_in[7], 1'b0};
+    wire rst=!rst_n;
+    tt_um_garage top (
+        .clk(clk),
+        .rst(rst),
+        .btn(ui_in[0]),
+        .ob(ui_in[1]),
+        .SA(ui_in[2]),
+        .SAB(ui_in[3]),
+        .MS(uo_out[0]),
+        .MB(uo_out[1])
+        );
 endmodule
